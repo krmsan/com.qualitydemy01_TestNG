db397282d3abe9a3f5162417bc555e1227167673
krmsan <sankrmsan@gmail.com>
2023-01-18 14:57:06 +0300

    yeni01

--- src/test/java/tests/us01/TC01_Login.java
+++ src/test/java/tests/us01/TC01_Login.java
@@ -11,11 +11,37 @@ public class TC01_Login {
 
     @Test
     public void test_login() {
-        login();
+        login(ConfigReader.getProperty("GecerliEmail"),ConfigReader.getProperty("GecerliPassword"));
         LoginPage pageLogin=new LoginPage();
         Assert.assertTrue(pageLogin.link_basariliGirisCourses.isDisplayed());
 
     }
+    @Test
+    public void test_loginInstructor() {
+        loginInsructor();
+       //
+        LoginPage pageLogin=new LoginPage();
+       // Assert.assertTrue(pageLogin.link_basariliGirisCourses.isDisplayed());
+
+    }
+
+    public void login(String email,String password) {
+
+
+        Driver.getDriver().get(ConfigReader.getProperty("Url"));
+
+        LoginPage pageLogin = new LoginPage();
+        pageLogin.cookieAccept.click();
+        pageLogin.link_homePageLogin.click();
+
+        pageLogin.input_kullaniciEmail.sendKeys(email);
+        pageLogin.input_password.sendKeys(password);
+
+        pageLogin.button_login.click();
+
+
+    }
+
 
     public void login() {
 
@@ -26,8 +52,42 @@ public class TC01_Login {
         pageLogin.cookieAccept.click();
         pageLogin.link_homePageLogin.click();
 
-        pageLogin.input_kullaniciEmail.sendKeys(ConfigReader.getProperty("GecerliUsername"));
+        pageLogin.input_kullaniciEmail.sendKeys(ConfigReader.getProperty("GecerliEmail"));
         pageLogin.input_password.sendKeys(ConfigReader.getProperty("GecerliPassword"));
+
+        pageLogin.button_login.click();
+
+
+    }
+
+    public void loginInsructor() {
+
+
+        Driver.getDriver().get(ConfigReader.getProperty("UrlBug"));
+
+        LoginPage pageLogin = new LoginPage();
+        pageLogin.cookieAccept.click();
+        pageLogin.link_homePageLogin.click();
+
+        pageLogin.input_kullaniciEmail.sendKeys(ConfigReader.getProperty("LoginInstructorEmail"));
+        pageLogin.input_password.sendKeys(ConfigReader.getProperty("GecerliPassword"));
+
+        pageLogin.button_login.click();
+
+
+    }
+
+    public void loginInstructor(String email,String password) {
+
+
+        Driver.getDriver().get(ConfigReader.getProperty("Url"));
+
+        LoginPage pageLogin = new LoginPage();
+        pageLogin.cookieAccept.click();
+        pageLogin.link_homePageLogin.click();
+
+        pageLogin.input_kullaniciEmail.sendKeys(email);
+        pageLogin.input_password.sendKeys(password);
 
         pageLogin.button_login.click();
 
