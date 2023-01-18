d3017e6ee8962948b5ec8074aa9ae36dd05f2a63
msdsn <devsanms@outlook.com>
2023-01-18 15:01:36 +0300

    xx

--- src/test/java/pages/instructor/HomePage.java
+++ src/test/java/pages/instructor/HomePage.java
@@ -1,5 +1,7 @@
 package pages.instructor;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilties.Driver;
 
@@ -7,6 +9,9 @@ public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(xpath = "")
+    public WebElement xxx;
 
 
 
